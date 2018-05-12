664b9cdee357e4cf2bd196f1c9d4d0289cf18f52
rajan jha <rajahit16@gmail.com>
2018-05-12 17:56:22 +0530

    defined all method

--- app/src/main/java/com/rajanapps/courtcounter/MainActivity.java
+++ app/src/main/java/com/rajanapps/courtcounter/MainActivity.java
@@ -3,10 +3,11 @@ package com.rajanapps.courtcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int teamAScore = 0;
-    int teamBScore = 0;
+    private  int teamAScore = 0;
+    private int teamBScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
      * @param score
      */
     private void displayTeamAScore(int score){
-
+        TextView team_a = findViewById(R.id.teamA_score);
+        team_a.setText(""+score);
     }
 
     /**
@@ -27,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
      * @param score
      */
     private void displayTeamBScore(int score){
-
+        TextView team_b = findViewById(R.id.teamB_score);
+        team_b.setText(""+score);
     }
 
     /**
@@ -36,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void add3ToAScore(View view) {
+        teamAScore += 3;
+        displayTeamAScore(teamAScore);
     }
 
     /**
@@ -43,20 +48,59 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void add2ToAScore(View view) {
+        teamAScore += 2;
+        displayTeamAScore(teamAScore);;
     }
+
+    /**
+     * add 1 to teamAScore;
+     * @param view
+     */
 
     public void add1ToAScore(View view) {
+        teamAScore += 1;
+        displayTeamAScore(teamAScore);
     }
+
+    /**
+     * add 3 to teamBScore
+     * @param view
+     */
 
     public void add3ToBScore(View view) {
+        teamBScore +=3;
+        displayTeamBScore(teamBScore);
     }
+
+    /**
+     * add 2 to teamBScore
+     * @param view
+     */
 
     public void add2ToBScore(View view) {
+        teamBScore += 2;
+        displayTeamBScore(teamBScore);
     }
+
+    /**
+     * add 1 to teamBScore.
+     * @param view
+     */
 
     public void add1ToBScore(View view) {
+        teamBScore +=1;
+        displayTeamBScore(teamBScore);
     }
 
+    /**
+     * reset both team score to zero and display.
+     * @param view
+     */
+
     public void resetScore(View view) {
+        teamAScore = 0;
+        teamBScore = 0;
+        displayTeamAScore(teamAScore);
+        displayTeamBScore(teamBScore);
     }
 }
